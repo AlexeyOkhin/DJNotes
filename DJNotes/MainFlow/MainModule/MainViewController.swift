3eea50127f5c6448a46ab94d4e8faf8cd43ffbe1
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-03-19 16:02:03 +0500

    add tableView with cell note

--- DJNotes/MainFlow/MainModule/MainViewController.swift
+++ DJNotes/MainFlow/MainModule/MainViewController.swift
@@ -11,8 +11,11 @@ class MainViewController: UIViewController {
 
     //MARK: - Private Properties
 
+    var notes = [NoteModel(title: "TestNote", description: "Test Description")]
+
     lazy var notesTableView: UITableView = {
         let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
 
@@ -20,11 +23,55 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemBackground
+        setupConstraints()
+        configureNavigationBar()
+        configureTableView()
+    }
+}
+
+//MARK: -  Private Methods
+
+private extension MainViewController {
+
+    func configureNavigationBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Notes"
     }
 
+    func setupConstraints() {
 
+        view.addSubview(notesTableView)
 
+        NSLayoutConstraint.activate([
+            notesTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            notesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            notesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            notesTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 
+    func configureTableView() {
+        notesTableView.dataSource = self
+        notesTableView.register(NoteTableViewCell.self, forCellReuseIdentifier: NoteTableViewCell.identifier)
+    }
 }
+
+//MARK: - UITableViewDataSource
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        notes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteTableViewCell.identifier, for: indexPath) as? NoteTableViewCell else { return UITableViewCell() }
+
+        let note = notes[indexPath.row]
+        cell.configure(note: note)
+        return cell
+    }
+}
+
+
 
---
+++ DJNotes/MainFlow/MainModule/NoteModel.swift
@@ -0,0 +1,14 @@
+//
+//  NoteModel.swift
+//  DJNotes
+//
+//  Created by Djinsolobzik on 19.03.2023.
+//
+
+import Foundation
+
+
+struct NoteModel {
+    let title: String
+    let description: String?
+}
